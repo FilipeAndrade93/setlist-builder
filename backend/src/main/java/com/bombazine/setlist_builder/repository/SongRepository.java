ef2300b15ca15a1107cf010fdf4cd0ee286fb1a6
Filipe Andrade <filipeandrade250693@gmail.com>
2026-03-08 11:31:11 +0000

    SongRepository created and ok

---
+++ backend/src/main/java/com/bombazine/setlist_builder/repository/SongRepository.java
@@ -0,0 +1,21 @@
+package com.bombazine.setlist_builder.repository;
+
+import com.bombazine.setlist_builder.entity.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, UUID> {
+
+    List<Song> findByDeletedAtIsNull();
+
+    List<Song> findByDeletedAtIsNullOrderByPopularityDesc();
+
+    Optional<Song> findBySpotifyIdAndDeletedAtIsNull();
+
+    boolean existsBySpotifyIdAndDeletedAtIsNull(String spotifyId);
+}
