3edf3daac301eb2449fe1fcc8c00da3c26147f25
Filipe Andrade <filipeandrade250693@gmail.com>
2026-03-09 19:27:33 +0000

    Spotify sync service complete

---
+++ backend/src/main/java/com/bombazine/setlist_builder/service/SpotifySyncService.java
@@ -0,0 +1,35 @@
+package com.bombazine.setlist_builder.service;
+
+import com.bombazine.setlist_builder.repository.SongRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SpotifySyncService {
+
+    private final SongRepository songRepository;
+    private final SpotifyClient spotifyClient;
+
+    @Scheduled(fixedRate = 86400000) //24 hours
+    @Transactional
+    public void syncPopularity() {
+        log.info("Spotify popularity syn init");
+        var tracks = spotifyClient.getTopTracks().join();
+        tracks.forEach(track -> songRepository.findBySpotifyIdAndDeletedAtIsNull(track.spotifyId())
+                .ifPresent(song -> {
+                    song.setPopularity(track.popularity());
+                    song.setPopularitySyncedAt(LocalDateTime.now());
+                    songRepository.save(song);
+                    log.debug("Updated popularity for: {}", song.getName());
+                }));
+
+        log.info("Spotify popularity sync finished");
+    }
+}
